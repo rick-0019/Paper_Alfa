48a6e1c44350bddc075fd783531d2becd56db43e
R S <rick-0019>
2026-07-26 17:41:14 -0300

    Add Paper_Alfa.bat launcher script for Windows

---
+++ Paper_Alfa.bat
@@ -0,0 +1,9 @@
+@echo off
+title Paper Alfa - CAD Spec A4 (1:1)
+echo ========================================================
+echo   PAPER ALFA - CAD Spec A4 (1:1)
+echo   Generador Parametrico de Papercraft Aeronautico
+echo ========================================================
+echo.
+echo Abriendo Paper Alfa en tu navegador predeterminado...
+start "" "%~dp0index.html"
